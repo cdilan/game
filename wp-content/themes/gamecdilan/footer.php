2763b60b98ab815ad1652ef7dd4a74b6c42ecae8
Mahmoud Aoki <mahaoki@gmail.com>
2012-11-26 10:59:45 -0200

    Meu dinamico
    menu dinamico no topo e rodapé

--- wp-content/themes/gamecdilan/footer.php
+++ wp-content/themes/gamecdilan/footer.php
@@ -1,7 +1,16 @@
-			<footer id="rodape">
-				<div class="container">
-					<p class="pull-right"><a href="#">Código de conduta</a> - <a href="#">Termos de uso</a> - <a href="#">Envie sua opinião</a></p>
-				</div>
+			
+			</section>
+			
+			<footer id="site-footer">
+					<section id="footer-menu">
+						<ul class="nav nav-pills pull-right">
+							<?php wp_nav_menu(array('theme_location' => 'footermenu', 'container' => false, 'items_wrap' => '%3$s', 'menu_id' => 'top-nav')); ?>
+			                <?php if(is_user_logged_in()) : ?>
+			                    <li class="logout"><a href="<?php echo wp_logout_url(); ?>" title="Logout" class=>Logout</a></li>
+			                <?php endif; ?>
+			            </ul>
+			        </section>
+		        </div>
 			</footer>
 		<?php wp_footer(); ?>
 	</body>
